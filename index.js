94e0f5b012192d41e51d75293030ecf1faafedf6
MykhCher <micher1240@gmail.com>
2024-07-03 12:41:21 +0300

    add .env conf

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require('http');
+require('dotenv').config();
+
 const app = require('./src/app');
+
+
+const port = process.env.PORT ?? 5000; 
+const hostname = process.env.HOSTNAME ?? '127.0.0.1'; 
 
 const server = http.createServer(app);
 
-server.listen(5000, '127.0.0.1');
+server.listen(port, hostname);
